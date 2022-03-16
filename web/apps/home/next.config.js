c9648e6d620ac207b1d2224241f7dcdbba2d4677
Aaron Miller <amiller@watheia.io>
2022-03-16 03:11:35 +0000

    disable multi-zone app for testing

--- web/apps/home/next.config.js
+++ web/apps/home/next.config.js
@@ -1,19 +1,21 @@
 const { withConfig, WA_EXPO_URL } = require('@watheia/next.utils');
 
-async function rewrites() {
-  return [
-    {
-      source: '/:path*',
-      destination: `/:path*`
-    },
-    {
-      source: '/expo',
-      destination: `${WA_EXPO_URL}`
-    },
-    {
-      source: '/expo/:path*',
-      destination: `${WA_EXPO_URL}/:path*`
-    }
-  ];
-}
-module.exports = withConfig({ rewrites });
+// async function rewrites() {
+//   return [
+//     {
+//       source: '/:path*',
+//       destination: `/:path*`
+//     },
+//     {
+//       source: '/expo',
+//       destination: `${WA_EXPO_URL}`
+//     },
+//     {
+//       source: '/expo/:path*',
+//       destination: `${WA_EXPO_URL}/:path*`
+//     }
+//   ];
+// }
+
+const nextConfig = {};
+module.exports = withConfig(nextConfig);
